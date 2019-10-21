45461ff1d29ba9b8931a6f0d9e5f461b6b25d0b5
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-10-21 00:44:34 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 28.0.0
    Change-Id: Id880093ac6fe13c831c126444fc660769722fdab

--- tests/phpunit/BSApiInsertFileLicenseStoreTest.php
+++ tests/phpunit/BSApiInsertFileLicenseStoreTest.php
@@ -15,7 +15,7 @@ class BSApiInsertFileLicenseStoreTest extends ApiTestCase {
 	/**
 	 * Anything that needs to happen before your tests should go here.
 	 */
-	protected function setUp() {
+	protected function setUp() : void {
 		// Be sure to do call the parent setup and teardown functions.
 		// This makes sure that all the various cleanup and restorations
 		// happen as they should (including the restoration for setMwGlobals).
