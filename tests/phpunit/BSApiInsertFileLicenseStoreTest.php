74306e82870a510f53eb083ec514685092a9abcf
DannyS712 <DannyS712.enwiki@gmail.com>
2020-02-15 02:55:00 +0000

    Remove use of ApiTestCase::doLogin
    Calling with no parameter results in using the test sysop, which is
    already used by default, so the calls can just be removed.
    
    Also removes empty setUp calls
    
    Bug: T245332
    Change-Id: Iedf8cd5b221c6b6af78766d38ac0b52d64ae43fe

--- tests/phpunit/BSApiInsertFileLicenseStoreTest.php
+++ tests/phpunit/BSApiInsertFileLicenseStoreTest.php
@@ -12,16 +12,6 @@
  * @group medium
  */
 class BSApiInsertFileLicenseStoreTest extends ApiTestCase {
-	/**
-	 * Anything that needs to happen before your tests should go here.
-	 */
-	protected function setUp() : void {
-		// Be sure to do call the parent setup and teardown functions.
-		// This makes sure that all the various cleanup and restorations
-		// happen as they should (including the restoration for setMwGlobals).
-		parent::setUp();
-		$this->doLogin();
-	}
 
 	/**
 	 * @covers \BSApiInsertFileLicenseStore::execute
